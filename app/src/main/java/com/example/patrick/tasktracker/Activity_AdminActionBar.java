3fe11efb677daea221c04a9426a3b867385b39ec
Andrew Humphrey <ah03926@georgiasouthern.edu>
2014-11-12 17:35:58 -0500

    Added Work Order Info Activity and xml layout

---
+++ app/src/main/java/com/example/patrick/tasktracker/Activity_AdminActionBar.java
@@ -0,0 +1,11 @@
+package com.example.patrick.tasktracker;
+
+import android.app.ActionBar;
+
+/**
+ * Created by Shwaat on 11/12/2014.
+ */
+public class Activity_AdminActionBar {
+
+
+}
--- app/src/main/java/com/example/patrick/tasktracker/Activity_AdminWoInfo.java
+++ app/src/main/java/com/example/patrick/tasktracker/Activity_AdminWoInfo.java
@@ -1,7 +1,15 @@
 package com.example.patrick.tasktracker;
 
+import android.os.Bundle;
+import android.app.Activity;
+
 /**
  * Created by Shwaat on 11/11/2014.
  */
-public class Activity_AdminWoInfo {
+public class Activity_AdminWoInfo extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_wo_info);
+    }
 }
