26050275ca906f45ca691d3cb64888dd68eac7cf
Andrew Humphrey <ah03926@georgiasouthern.edu>
2014-11-16 15:21:59 -0500

    correct actionbar on admin_wo_info, also changed title in the Manifest.

--- app/src/main/java/com/example/patrick/tasktracker/Activity_AdminWoInfo.java
+++ app/src/main/java/com/example/patrick/tasktracker/Activity_AdminWoInfo.java
@@ -92,7 +92,7 @@ public class Activity_AdminWoInfo extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
 // Inflate the menu items for use in the action bar
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_actionbar, menu);
+        inflater.inflate(R.menu.wo_actionbar, menu);
         return super.onCreateOptionsMenu(menu);
     }
 }
