98af009a0beef5e062f89c63759607937a64b957
Andrew Humphrey <ah03926@georgiasouthern.edu>
2014-11-15 19:59:49 -0500

    activity employee info layout

---
+++ app/src/main/java/com/example/patrick/tasktracker/Activity_EmployeeInfo.java
@@ -0,0 +1,16 @@
+package com.example.patrick.tasktracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Shwaat on 11/15/2014.
+ */
+public class Activity_EmployeeInfo extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_employee_info);
+    }
+}
