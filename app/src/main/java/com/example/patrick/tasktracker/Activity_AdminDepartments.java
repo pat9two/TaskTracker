f5ecb0dc492c93434cf4f8481cb72b9a7f23af45
Patrick Marino <patrickmarino2014@gmail.com>
2014-12-07 15:15:38 -0500

    commented AdminDepartments

--- app/src/main/java/com/example/patrick/tasktracker/Activity_AdminDepartments.java
+++ app/src/main/java/com/example/patrick/tasktracker/Activity_AdminDepartments.java
@@ -26,9 +26,6 @@ import com.parse.ParseQueryAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Shwaat on 11/2/2014.
- */
 public class Activity_AdminDepartments extends ActionBarActivity {
 
     ListView departmentListView;
@@ -38,28 +35,37 @@ public class Activity_AdminDepartments extends ActionBarActivity {
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+        //line to allow activity to use parse database.
         Parse.initialize(this, "6yEsCcvYy5ym7rmRKWleVy5A9jc2wHFz6aEL3Czs", "t3h3S0090VVBwdw0zasj5J0b28dLe9xebL5nIfKw");
         super.onCreate(savedInstanceState);
 
         Log.d("Department", "main");
+        //xml file to use.
         setContentView(R.layout.admin_department_main);
 
-
+        //adapter to populate a list view.
         mainAdapter = new ParseQueryAdapter<ParseObject>(this, "Department");
+        //value that will be shown in each list item of the list view.
         mainAdapter.setTextKey("Department_id");
 
 
         departmentListView = (ListView)findViewById(R.id.departments_list_view);
+        //sets padding on the xml element.
         departmentListView.setPadding(0,5,0,5);
         departmentListView.setAdapter(mainAdapter);
         mainAdapter.loadObjects();
 
+        //sets listener when the user presses on a list item.
         departmentListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //gets the values from the parse object that was pressed.
                 ParseObject po = (ParseObject)parent.getItemAtPosition(position);
+                //creates department object from those values.
                 Department dep = new Department(po.getObjectId(), po.get("Department_id").toString(), po.get("Charged").toString());
+                //log statement
                 Log.d("AdminDepartments", "Charged status is : " + po.getString("Charged"));
+                //passes the data to the next activity.
                 Intent intent = new Intent(view.getContext(), Activity_DepartmentLocInfo.class);
                 intent.putExtra("extra", dep);
                 Log.d("AdminDepartments", " " +po.getObjectId());
@@ -68,24 +74,28 @@ public class Activity_AdminDepartments extends ActionBarActivity {
         });
     }
 
+    //on click button method to add a new department. Navigates to next activity.
     public void addDepartment(View view){
         Intent intent = new Intent(this, Activity_AdminNewDepartment.class);
         startActivity(intent);
     }
 
+    //on click button method to remove a department. Navigates to next activity.
     public void remDepartment(View view){
         Intent intent = new Intent(this, Activity_DepartmentRemLoc.class);
         startActivity(intent);
     }
 
+    //required method for actionbar.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-// Inflate the menu items for use in the action bar
+        // Inflate the menu items for use in the action bar
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_actionbar, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
+    //required method for actionbar.
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
